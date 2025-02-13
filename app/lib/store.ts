aeb2d0e7f163aa24ebd14caa35db84155538c2d6
eliguy343 <eliguy343@gmail.com>
2025-02-13 21:48:00 +0200

    redux

---
+++ app/lib/store.ts
@@ -0,0 +1,41 @@
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { useDispatch, useSelector, useStore } from "react-redux";
+import { UserData } from "../types/ProductTyoe";
+
+const initialUserData : UserData = {
+  loggedIn: false
+}
+
+const userDataSlice = createSlice({
+  name:"UserData",
+  initialState: initialUserData,
+  reducers: {
+    loginUser(state, action: PayloadAction<UserData>) {
+      state = action.payload;
+      return state;
+    },
+    logoutUser(state) {
+      state = {
+        loggedIn: false
+      }
+      return state;
+    }
+  }
+});
+
+export const makeStore = () => {
+  return configureStore({
+    reducer:{
+      userData: userDataSlice.reducer
+    }
+  })
+}
+
+export const {loginUser, logoutUser} = userDataSlice.actions;
+
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppStore = useStore.withTypes<AppStore>()
---
+++ app/types/ProductTyoe.ts
@@ -0,0 +1,7 @@
+export interface UserData {
+  email?: string,
+  address?: string,
+  name ?: string,
+  role?: string
+  loggedIn: boolean
+}
