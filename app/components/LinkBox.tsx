e3d4b519171b859d0e70efc85e7eda38b0b72498
eliguy343 <eliguy343@gmail.com>
2025-02-21 22:16:10 +0200

    main page

---
+++ app/components/LinkBox.tsx
@@ -0,0 +1,14 @@
+import Image, { StaticImageData } from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const LinkBox = ({title, link, image} : {title: string, link: string, image:string | StaticImageData})  => {
+  return (
+    <Link href={link} className=' rounded-xl bg-slate-800 hover:bg-slate-700 active:bg-slate-600 transition-all h-auto p-5 flex flex-col border-2 border-white hover:cursor-pointer items-center'>
+      <h2 className='text-white text-2xl font-bold'>{title}</h2>
+      <Image src={image} alt={title} className='w-10 h-10'/>
+    </Link>
+  )
+}
+
+export default LinkBox
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,31 @@ import Image from "next/image";
 import ProdcutIcon from '../assets/box.png'
 import GrowthIcon from '../assets/growth.png'
 import storeManagementLogo from '../assets/management.png';
+import LinkBox from "./components/LinkBox";
 
 export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col items-center">
       <div className="w-full flex flex-col items-center justify-start">
-        <h2 className="text-white font-bold text-5xl uppercase"> Store Management</h2>
+        <h2 className="text-white font-bold text-xl md:text-5xl uppercase"> Store Management</h2>
         <Image src={storeManagementLogo} alt="logo" className="w-[8%] m-5"/>
       </div>
-      <div className="w-[90%] grid grid-cols-2 gap-5 p-5">
-        
+      <div className="w-full md:w-[60%] grid grid-cols-1 md:grid-cols-2 gap-5 p-5">
+        <LinkBox  
+          title={"Products"}
+          link={'/products'}
+          image={ProdcutIcon}
+        />
+        <LinkBox
+          title={"Sales"}
+          link={'/sales'}
+          image={GrowthIcon}
+        />
+        <LinkBox
+          title={"Categories"}
+          link={'/category'}
+          image={GrowthIcon}
+        />
       </div>
     </div>
   );
