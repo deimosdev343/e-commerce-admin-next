e8a2a8afd39ec67ebb8fcaf192e2c064c0026b6b
eliguy343 <eliguy343@gmail.com>
2025-02-27 23:27:36 +0200

    prd colors sizes

--- app/components/products/ProductBox.tsx
+++ app/components/products/ProductBox.tsx
@@ -14,14 +14,37 @@ const ProductBox = ({product} : {product: ProductType}) => {
         <Image
           alt='product image'
           src={fallback ? PictureLogo : product.image }
-          onError={(err) => {setFallback(true)}}
+          onError={() => {setFallback(true)}}
           width={360}
           height={360}
-          className='border-gray-300 border-4 rounded-md'
+          className='border-gray-300 border-4'
         />
         <h2 className='text-white font-bold text-lg'>
           ID: {product._id}
         </h2> 
+      </div>
+      <div className='flex flex-col h-full justify-between gap-2 p-5'>
+        <h2 className='text-white font-bold text-2xl'>
+          {product.name}
+        </h2>
+        <div className='flex w-full items-center gap-5'>
+          <h2 className='text-white font-bold text-2xl'>
+            Colors: [
+          </h2>
+          {product.colors.map(clr => <div
+            key={clr}
+            className='flex flex-row p-3 rounded-xl shadow-lg'
+            style={{backgroundColor:clr}}
+          > 
+          </div>)}
+          <h2 className='text-white font-extrabold text-2xl'>]</h2>
+        </div>
+        <div className='w-full flex items-center gap-2'>     
+          <h2 className='text-white font-extrabold text-2xl'> sizes: [</h2>
+          {product.sizes.map(sz => <h2 key={sz} className='text-2xl font-bold'>{sz}</h2>)}
+
+          <h2 className='text-white font-extrabold text-2xl'> ]</h2>
+        </div> 
       </div>
     </div>
   )
