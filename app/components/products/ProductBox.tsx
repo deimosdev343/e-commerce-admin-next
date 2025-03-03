b4da4e9298ba5643adffb09587dcee8ef544df13
eliguy343 <eliguy343@gmail.com>
2025-03-03 22:55:00 +0200

    btn

--- app/components/products/ProductBox.tsx
+++ app/components/products/ProductBox.tsx
@@ -9,8 +9,8 @@ import Image from 'next/image'
 const ProductBox = ({product} : {product: ProductType}) => {
   const [fallback, setFallback] = useState<boolean>(false);
   return (
-   <div className="p-5 flex w-full bg-slate-700 rounded-xl border-2 border-gray-500">
-     <div className="flex flex-col h-full justify-center items-center">
+   <div className="p-5 flex w-full items-center bg-slate-700 rounded-xl border-2 border-gray-500">
+     <div className="w-[33%] flex flex-col h-full justify-center items-center">
         <Image
           alt='product image'
           src={fallback ? PictureLogo : product.image }
@@ -23,12 +23,12 @@ const ProductBox = ({product} : {product: ProductType}) => {
           ID: {product._id}
         </h2> 
       </div>
-      <div className='flex flex-col h-full justify-between gap-2 p-5'>
+      <div className='w-[28%] flex flex-col h-full items-start justify-start gap-2 '>
         <h2 className='text-white font-bold text-2xl'>
           {product.name}
         </h2>
         <h2 className='text-white font-bold text-xl italic'>
-          {product.description.length > 20 ? product.description.substring(0, 40) + "..." : product.description }
+          {product.description.length > 40 ? product.description.substring(0, 40) + "..." : product.description }
         </h2>
         <div className='flex w-full items-center gap-5'>
           <h2 className='text-white font-bold text-2xl'>
@@ -48,6 +48,14 @@ const ProductBox = ({product} : {product: ProductType}) => {
 
           <h2 className='text-white font-extrabold text-2xl'> ]</h2>
         </div> 
+      </div>
+      <div className='w-[33%] gap-5 h-full flex flex-col justify-center items-center '>
+        <button className='w-[60%] border-white bg-slate-900 border-2 p-2 rounded-lg font-bold text-2xl shadow-lg hover:bg-slate-800'>
+          Edit Item
+        </button>
+        <button className='w-[60%] border-white border-2 bg-slate-900 p-2 rounded-lg font-bold text-2xl shadow-lg hover:bg-slate-800'>
+          Delete Item
+       </button>
       </div>
     </div>
   )
