0e684ed2ccf9729ee1a35d830181d0cff783be51
eliguy343 <eliguy343@gmail.com>
2025-03-19 22:35:49 +0200

    edit prod md

--- components/products/modals/EditProduct.tsx
+++ components/products/modals/EditProduct.tsx
@@ -9,16 +9,23 @@ const EditProduct = ({modalState, setModalState} :{
 
   return (
     <Modal 
-      show={modalState.show} 
+      show={modalState.show}
+      className='bg-black'
       onClose={() => {
         setModalState({show: false, product: null});
       }}
     >
-     <Modal.Header>
-      <h2 className='text-2xl font-bold text-white'>
+     <Modal.Header
+      className='bg-gray-600 border-b-0 shadow-xl'
+
+     >
+      <h2 className='text-3xl font-bold text-white '>
         Edit Product
       </h2>
      </Modal.Header>
+     <Modal.Body className='bg-gray-600  shadow-xl'>
+
+     </Modal.Body>
     </Modal>
   )
 }
