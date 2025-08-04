81362e39bb50aac857a1b82f5900cf76f4504321
Eli Guy <eliguy343@gmail.com>
2025-08-04 19:53:55 +0300

    nextjs routing is abysmal dgshit

--- components/statistics/MostViewedItemsComponent.tsx
+++ components/statistics/MostViewedItemsComponent.tsx
@@ -1,6 +1,15 @@
+import axios from "axios";
 import { useEffect } from "react"
 
 const MostViewedItemsComponent = () => {
+  
+  const fetchMostViewed = async () => {
+    try {
+      const res = await axios.get('/api/statistics')    
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   useEffect(() => {
 
