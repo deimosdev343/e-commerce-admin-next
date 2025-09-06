f399dd0ad52e9fd97e8aead2655cef972a290348
eliguy343 <eliguy343@gmail.com>
2025-09-06 19:21:30 +0300

    prd styling

--- components/products/ProductBox.tsx
+++ components/products/ProductBox.tsx
@@ -16,7 +16,7 @@ const ProductBox = ({
   setDeleteModal: Function}) => {
   const [fallback, setFallback] = useState<boolean>(false);
   return (
-   <div className="p-5 flex w-full items-center bg-slate-700 rounded-xl border-2 border-gray-500">
+   <div className="p-5 flex w-[99%] items-center bg-white rounded-xl border-2 shadow-md">
      <div className="w-[33%] flex flex-col h-full justify-center items-center">
         <Image
           alt='product image'
@@ -26,23 +26,23 @@ const ProductBox = ({
           height={360}
           className='border-gray-300 border-4'
         />
-        <h2 className='text-white font-bold text-lg'>
+        <h2 className='text-black font-bold text-lg'>
           ID: {product._id}
         </h2> 
       </div>
       <div className='w-[28%] flex flex-col h-full items-start justify-start gap-2 '>
-        <h2 className='text-white font-bold text-2xl'>
+        <h2 className='text-black font-bold text-2xl'>
           {product.name}
         </h2>
-        <h2 className='text-white font-bold text-xl italic'>
+        <h2 className='text-black font-bold text-xl italic'>
           {product.description.length > 40 ? product.description.substring(0, 40) + "..." : product.description }
         </h2>
         <div className='flex flex-col w-full items-start gap-5 '>
-          <h2 className='text-white font-bold text-3xl'>
+          <h2 className='text-black font-bold text-3xl'>
             Colors 
           </h2>
           
-          <div className='grid grid-cols-5 gap-5  items-center bg-gray-700 p-2 shadow-xl rounded-md border-2 border-white '>
+          <div className='grid grid-cols-5 gap-5  items-center bg-white p-2 shadow-xl rounded-md border-2 border-slate-400 '>
             {product.colors.map(clr => <div
               key={clr}
               className='flex flex-row p-3 rounded-xl shadow-lg border-white '
@@ -54,16 +54,16 @@ const ProductBox = ({
           
         </div>
         <div className='w-full flex items-center gap-2'>     
-          <h2 className='text-white font-extrabold text-2xl'> sizes: [</h2>
-          {product.sizes.map(sz => <h2 key={sz} className='text-2xl font-bold text-white'>{sz}</h2>)}
+          <h2 className='text-black font-extrabold text-2xl'> sizes: [</h2>
+          {product.sizes.map(sz => <h2 key={sz} className='text-2xl font-bold text-black'>{sz}</h2>)}
 
-          <h2 className='text-white font-extrabold text-2xl'> ]</h2>
+          <h2 className='text-black font-extrabold text-2xl'> ]</h2>
         </div> 
       </div>
       <div className='w-[33%] gap-5 h-full flex flex-col justify-center items-center '>
         <button 
-          className='w-[60%] border-white bg-slate-900 border-2 p-2 rounded-lg
-            font-bold text-2xl shadow-lg hover:bg-slate-800 text-white'
+          className='w-[60%]  bg-white  p-2 rounded-lg border-2 border-slate-400
+            font-bold text-2xl shadow-lg hover:bg-slate-100 text-black'
           onClick={() => {
             setEditModal({show:true, product: {...product}, type: "Edit"})
           }}
@@ -72,9 +72,9 @@ const ProductBox = ({
         </button>
         <button 
           className='
-            w-[60%] border-white border-2 bg-slate-900 p-2 
+            w-[60%] border-2 border-slate-400 bg-white p-2 
             rounded-lg font-bold text-2xl shadow-lg
-          text-white hover:bg-slate-800'
+          text-black hover:bg-slate-100'
           onClick={() => {
             setDeleteModal({
               id: product._id,
