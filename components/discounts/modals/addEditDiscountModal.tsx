b6d3f5fbeedea709eb09349bf24224181eafdc58
eliguy343 <eliguy343@gmail.com>
2025-11-19 23:38:30 +0200

    discount image

--- components/discounts/modals/addEditDiscountModal.tsx
+++ components/discounts/modals/addEditDiscountModal.tsx
@@ -1,9 +1,12 @@
 "use client"
+import UploadImageIcon from '../../../assets/photo.png';
 
 import { discountType } from '@/types/DiscountType'
 import { Datepicker, Modal, Textarea, TextInput } from 'flowbite-react'
 import React from 'react'
 import dayjs from 'dayjs';
+import { CldUploadWidget, CloudinaryUploadWidgetInfo } from 'next-cloudinary';
+import Image from 'next/image';
 
 interface modalStateType {
   show: boolean | undefined,
@@ -75,6 +78,44 @@ const AddEditDiscountModal = ({modalState, setModalState}:{
               }}
             />
           </div>
+        </div>
+        <div className="w-full flex flex-col justify-start items-start p-2 gap-2">
+          <h2 className='font-bold text-2xl text-black'>
+            Image
+          </h2>
+          <CldUploadWidget
+            signatureEndpoint="/api/upload"
+            onSuccess={({info, event}, { widget }) => {
+              setModalState((mdlstate: modalStateType) => 
+                ({...mdlstate,
+                  discount: {...mdlstate.discount, image: (info as CloudinaryUploadWidgetInfo).secure_url}}));  
+            }}
+            onQueuesEnd={(result, { widget }) => {
+              widget.close();
+            }}
+          >
+            {({ open }) => {
+              function handleOnClick() {
+                open();
+              }
+              return (
+                <button className='flex w-full relative' onClick={handleOnClick}>
+                  <Image
+                    src={UploadImageIcon}
+                    alt='Upload Image'
+                    className='top-3 left-0 absolute w-9 z-10'
+                  />
+                </button>
+              );
+            }}
+          </CldUploadWidget>
+          <TextInput 
+            className="pl-10 w-full font-semibold"
+            value={modalState.discount?.image} 
+            onChange={(e) => setModalState((
+              mdlstate: modalStateType ) => (
+                {...mdlstate, discount: {...mdlstate.discount, image: e.target.value}}))}
+          />
         </div>
         <div className="w-full flex flex-col  p-2 gap-2">
           <h2 className='text-black font-bold text-lg'>Image Background</h2>
