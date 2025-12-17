7a6d48d7f7c8ba8e585a378104c3bda78428600f
eliguy343 <eliguy343@gmail.com>
2025-12-17 23:28:27 +0200

    background

--- components/discounts/DiscountComponent.tsx
+++ components/discounts/DiscountComponent.tsx
@@ -1,5 +1,7 @@
 import { DiscountType } from '@/types/DiscountType'
-import React from 'react'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import PictureLogo from '../../assets/ImageLogo.png'
 
 type DiscountComponentType =  {
   discount: DiscountType,
@@ -8,9 +10,24 @@ type DiscountComponentType =  {
 }
 
 const DiscountComponent = ({discount, setEditModal, setDeleteModal} : DiscountComponentType) => {
+  const [fallback, setFallback] = useState<boolean>(false);
+  
   return (
     <div className='p-5 w-full items-center bg-white rounded-xl border-2 shadow-md'>
-      <p>test</p>
+      <div className='w-[33%] flex flex-col h-full justify-center items-center gap-2'>
+        <Image
+          alt="discount image"
+          title='discount image'
+          src={fallback ? PictureLogo : discount.image}
+          onError={() => {setFallback(true)}}
+          width={360}
+          height={360}
+          className='border-gray-800 border-4 rounded-md'
+        />
+        <h2 className='font-bold'>Background:</h2>
+        <div className={`h-[10%] w-[360] p-5 ${discount.background} border-gray-800 border-4`}>
+        </div>
+      </div>
     </div>
   )
 }
