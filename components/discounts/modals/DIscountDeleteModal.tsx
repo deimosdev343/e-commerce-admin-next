40a608763e16fb6395179db0767316064f9fcdd5
eliguy343 <eliguy343@gmail.com>
2025-12-19 15:26:10 +0200

    discount modal func

--- components/discounts/modals/DIscountDeleteModal.tsx
+++ components/discounts/modals/DIscountDeleteModal.tsx
@@ -1,6 +1,27 @@
+import axios from 'axios';
 import React from 'react'
 
-const DIscountDeleteModal = () => {
+interface modalStateType {
+  id: string,
+  show: boolean | undefined,
+}
+
+const DIscountDeleteModal = (
+  {modalState, setModalState}: 
+  {modalState: modalStateType, setModalState: Function}) => {
+  
+  const onDelete = async () => {
+    try {
+      await axios.delete('/api/discounts',{
+        params:{
+          discountId:modalState.id
+        }
+      });
+      setModalState({id:null, show: false});
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div>DIscountDeleteModal</div>
   )
