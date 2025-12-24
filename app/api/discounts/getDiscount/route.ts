2cc542bbe71e3e25bb5adf2e60098a409189d69e
eliguy343 <eliguy343@gmail.com>
2025-12-25 00:00:27 +0200

    get discount api

---
+++ app/api/discounts/getDiscount/route.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest, res: NextResponse) => {
+  try {
+    const searchParams = req.nextUrl.searchParams;
+    const discountRes = await axios.get(
+      `${process.env.BACKEND_API}/discounts/getDiscount`, 
+      {
+        params:{
+          discountId: searchParams.get("discountId")
+        }
+      }
+    );
+    return NextResponse.json(
+      {discount: discountRes.data},
+      {status:200}
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      {msg:"Internal Server Error"},
+      {status:500}
+    );
+  }
+}
