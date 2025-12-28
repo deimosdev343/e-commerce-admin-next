cd4140dc60d0c7bdc3cde7f7081bb51007424c40
Eli Guy <eliguy343@gmail.com>
2025-12-28 20:42:19 +0200

    get disount products

---
+++ app/api/discounts/getDiscountProducts/route.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest, res:NextResponse) => {
+  try {
+    const searchParams = req.nextUrl.searchParams;
+    const discountRes = await axios.get(
+      `${process.env.BACKEND_API}/discounts/getDiscount`, 
+      {
+        params:{
+          discountId: searchParams.get("discountId")
+        }
+      }
+    );
+    return NextResponse.json(
+      discountRes.data,
+      {status:200}
+    );
+  } catch (err) {
+    return NextResponse.json(
+      {msg:"internal Server Error"},
+      {status:500}
+    );
+  }
+}
