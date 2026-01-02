f14a05a3be6908122c552514b855cc802cb09067
eliguy343 <eliguy343@gmail.com>
2026-01-02 12:03:05 +0200

    Middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -3,23 +3,12 @@ import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 import { getAuth } from './app/services/getAuth';
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const cks = await cookies();
   const token = cks.get("token")?.value;
-
   if(!token && req.nextUrl.pathname != '/login') {
     return NextResponse.redirect(new NextURL('/login', req.nextUrl));
   }
-  // TODO: figure out something better than this
-  // try {
-  //   await getAuth(String(token));
-  // } catch (err) {
-  //   cks.delete('token');
-  //   console.log(err);
-  //   return NextResponse.redirect(new NextURL('/login', req.nextUrl));
-  // }
-  
-
   return NextResponse.next();
 }
 
