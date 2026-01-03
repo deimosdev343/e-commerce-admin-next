825dbcfd1edbcde12dc1e53c7f64596d465242d0
eliguy343 <eliguy343@gmail.com>
2026-01-03 18:23:16 +0200

    Login page mobile view

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -34,17 +34,17 @@ const LoginPage = () => {
     <div className='w-screen h-screen   flex flex-col justify-center items-center'>
 
       <form 
-        className='w-[50%] p-5 shadow-xl bg-white h-[50%] flex flex-col  
-          items-center justify-between rounded-3xl border-slate-600 border-[4px]'
+        className='w-[95%] md:w-[50%] p-5 shadow-xl bg-white h-[90%] md:h-[85%] lg:h-[60%] flex flex-col  
+          items-center justify-between rounded-3xl border-slate-600 border-[4px] overflow-clip'
         onSubmit={handleSubmit(onSubmit)}
       >
         <div className='w-full flex flex-col justify-center items-center'>
-          <h2 className='font-bold text-3xl text-black'>Control Panel Login</h2>
-          <h2 className='font-semibold text-lg text-gray-700'>Your credentials should be provided by the service provider</h2>
+          <h2 className='font-bold text-xl text-black'>Managment Login</h2>
+          <h2 className='font-semibold text-sm text-gray-700'>Your credentials should be provided by the service provider</h2>
         </div>
         <div className='w-full flex flex-col justify-center items-center'>
           <div className='w-[100%] flex flex-col justify-center items-center p-2 gap-2'>
-            <h2 className='text-2xl font-bold text-black'>email</h2>
+            <h2 className='text-lg font-bold text-black'>Username</h2>
             <input 
               className={`text-black p-2 w-[50%] bg-white border-2 ${errors?.email?.type ? "border-red-500" : "border-black"} border-black rounded-lg`} 
               {...register("email", {required: true})}
@@ -54,7 +54,7 @@ const LoginPage = () => {
             </h2>}
           </div>
           <div className='w-full flex flex-col justify-center items-center p-2 gap-2'>
-            <h2 className='text-2xl font-bold text-black'>Password</h2>
+            <h2 className='text-lg font-bold text-black'>Password</h2>
             <input 
               className={`text-black p-2 w-[50%] bg-white border-2 ${errors.password?.type ? "border-red-500" : "border-black"} rounded-lg`}
               {...register("password", {required: true})}
@@ -66,7 +66,7 @@ const LoginPage = () => {
           </div>
         </div>
 
-        <button className='bg-blue-500 w-[50%] px-3 py-2 rounded-xl text-white text-2xl font-extrabold '>
+        <button className='bg-blue-500 w-[50%] px-3 py-2 rounded-xl text-white text-md font-extrabold '>
           Login
         </button>
       </form>
